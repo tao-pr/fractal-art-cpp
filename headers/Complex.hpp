84870ec5e90a08707538f4e3c11f2474fa9c85a1
Tao Ruangyam <tao.ruangyam@zalando.de>
2024-01-03 11:12:39 +0100

    cleanup

--- headers/Complex.hpp
+++ headers/Complex.hpp
@@ -3,25 +3,6 @@
 #include <math.h>
 #include <opencv2/core/types.hpp>
 
-// class Complex
-// {
-// public:
-//   double r;
-//   double i;
-//   Complex(const double &r, const double &i) : r(r), i(i){};
-//   ~Complex(){};
-//   double l2norm() const { return sqrt(r * r + i * i); };
-//   Complex square() const
-//   {
-//     return Complex(r * r - i * i, 2 * r * i);
-//   };
-
-//   Complex operator+(const Complex &z) const
-//   {
-//     return Complex(r + z.r, i + z.i);
-//   };
-// };
-
 using namespace cv;
 
 namespace Z
