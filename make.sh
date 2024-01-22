33b37a9f090f57aef01eeb011f2985dd5f0c0b93
Tao PR <pataoengineer@gmail.com>
2024-01-22 11:46:46 +0100

    make script builds for both x86 and ARM

--- make.sh
+++ make.sh
@@ -11,15 +11,23 @@ export LLVM_LIB_DIR="$(llvm-config --libdir) $(llvm-config --libdir)/c++"
 export LLVM_INCLUDE_DIR=$(llvm-config --includedir)/c++/v1
 export LLVM_ROOT=$(brew --prefix llvm@16) # for cmakelist
 
-# taotodo make this compatible with both arm64 and x86_64
+if [ $(uname -m) = "x86_64" ]; then
+  ARCH="x86_64"
+  TARGET="x86_64"
+  echo "Building for Intel x86_64"
+else
+  ARCH="arm64"
+  TARGET="aarch64"
+  echo "Building for ARM64"
+fi
 
 cmake -DCMAKE_C_COMPILER=$(brew --prefix llvm@16)/bin/clang \
       -DCMAKE_CXX_COMPILER=$(brew --prefix llvm@16)/bin/clang++ \
       -DCMAKE_OSX_SYSROOT=$(xcrun --show-sdk-path) \
-      -DCMAKE_OSX_ARCHIECTURES='arm64' \
+      -DCMAKE_OSX_ARCHIECTURES="$ARCH" \
       -DDEFAULT_SYSROOT=$(xcrun --show-sdk-path) \
-      -DCMAKE_OSX_ARCHITECTURES='arm64' \
-      -DLLVM_TARGETS_TO_BUILD='aarch64' \
+      -DCMAKE_OSX_ARCHITECTURES="$ARCH" \
+      -DLLVM_TARGETS_TO_BUILD="$TARGET" \
       -DLLVM_DEFAULT_TARGET_TRIPLE=$(llvm-config --host-target) \
       -DCMAKE_BUILD_TYPE=Release \
       -DLLVM_BUILD_RUNTIME=Off \
